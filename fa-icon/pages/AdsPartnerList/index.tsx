62a42f0b7fa477324f058dfa670586997e5a48c2
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-03-24 15:18:38 +0530

    FEAT:add patner list page created

---
+++ fa-icon/pages/AdsPartnerList/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+import React from "react";
+
+const AdsPartnerList: NextPage = ({}) => {
+  return <h1>Ads Partner List</h1>;
+};
+export default React.memo(AdsPartnerList);
