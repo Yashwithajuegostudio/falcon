5bc27a06244ef6ecaa441b82065837fe12849d13
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-03-24 15:18:06 +0530

    FEAT: add device id page created

---
+++ fa-icon/pages/AddDeviceId/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+import React from "react";
+
+const AddDeviceId: NextPage = ({}) => {
+  return <h1>Add Device Id</h1>;
+};
+export default React.memo(AddDeviceId);
